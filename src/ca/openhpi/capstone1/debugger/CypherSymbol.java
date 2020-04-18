63817b32400ab9c936011c32b5ce8a57dcf2421a
nano <nano@nanoTHINKw530>
2020-04-18 12:50:06 -0400

    first commit

---
+++ src/ca/openhpi/capstone1/debugger/CypherSymbol.java
@@ -0,0 +1,11 @@
+package ca.openhpi.capstone1.debugger;
+
+public enum CypherSymbol {
+	/* This Enum does not has to be changed.
+	 * A CypherSymbol may be short, long or a break (blank).
+	 * A MorseCharacter consists of a combination of CypherSymbols.
+	 */
+	SHORT,
+	LONG,
+	BLANK
+}
---
+++ src/ca/openhpi/capstone1/starter/Main.java
@@ -0,0 +1,11 @@
+package ca.openhpi.capstone1.starter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Eclipse");
+
+	}
+
+}
